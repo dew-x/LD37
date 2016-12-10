0813ede5b8ececdc3f9211c2adae25645c672bbd
dew-x <afigueras@enti.cat>
2016-12-10 17:42:28 +0100

    jump

--- LD37/Assets/character.js
+++ LD37/Assets/character.js
@@ -2,10 +2,12 @@
 
 var character : GameObject;
 var rb : Rigidbody;
+var canJump : int;
 
 function Start () {
 	Physics.gravity = Vector3(0,-100,0);
 	rb = character.GetComponent.<Rigidbody>();
+	canJump = 0;
 }
 
 function Update () {
@@ -23,10 +25,16 @@ function Update () {
 	if (Input.GetKey("s")) {
 		y -= 1;
 	}
-	if (Input.GetKeyDown("space")) {
+	if (Input.GetKeyDown("space") && canJump>0) {
 		rb.AddForce(0,2000,0);
+		canJump = -100;
 	}
 	rb.AddForce(x*50,0,y*50);
 
 	rb.AddForce(rb.velocity*-0.05);
+	++canJump;
+}
+
+function OnCollisionStay(){
+   if (canJump>-80) canJump=0;
 }
