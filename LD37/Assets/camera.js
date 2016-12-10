13c7e9bcfa9f0700d6e7c56fe337d093aec4778d
dew-x <afigueras@enti.cat>
2016-12-10 16:40:19 +0100

    subworking

--- LD37/Assets/camera.js
+++ LD37/Assets/camera.js
@@ -4,8 +4,11 @@ var character : GameObject;
 var cam : Camera;
 
 function Start () {
+	cam.transform.position = Vector3(0,0,0);
 
 }
 
 function Update () {
+	//cam.transform.position = Vector3.Scale(character.transform.position,Vector3(0,0,1)) + Vector3(0,120,-80);
+	cam.transform.LookAt(character.transform.position);
 }
