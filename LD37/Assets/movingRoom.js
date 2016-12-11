20fc50da77dcd46692e350ec6645c611db233a36
dew-x <afigueras@enti.cat>
2016-12-11 10:22:32 +0100

    walls collision removed

--- LD37/Assets/movingRoom.js
+++ LD37/Assets/movingRoom.js
@@ -12,6 +12,19 @@ function Update () {
 
 function OnCollisionEnter(collision: Collision) {
 	if (collision.gameObject.name=="Character") {
-		 UnityEngine.SceneManagement.SceneManager.LoadScene("main", UnityEngine.SceneManagement.LoadSceneMode.Single);
+		for (var contact : ContactPoint in collision.contacts) {
+			if (Mathf.Abs(contact.normal.z)<0.01) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene("main", UnityEngine.SceneManagement.LoadSceneMode.Single);
+			}
+		}
+	}
+}
+function OnCollisionStay(collision: Collision) {
+	if (collision.gameObject.name=="Character") {
+		for (var contact : ContactPoint in collision.contacts) {
+			if (Mathf.Abs(contact.normal.z)<0.01) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene("main", UnityEngine.SceneManagement.LoadSceneMode.Single);
+			}
+		}
 	}
 }
