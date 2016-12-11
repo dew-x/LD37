675eb65571a7f14ecd7414a21546304d7b2350df
spros <s.pros@hotmail.com>
2016-12-11 16:28:12 +0100

    Time Changes

--- LD37/Assets/scoreManager.js
+++ LD37/Assets/scoreManager.js
@@ -1,30 +1,48 @@
 ﻿#pragma strict
 
-var myStyle: GUIStyle;
+var myStyle : GUIStyle;
+var wasdImage : Texture2D;
 
-private var timer1:int;
+static var time : int;
+private var timer2 : int;
+private var tutorial : boolean;
 
 function Start () {
-
-	timer1 = 0;
-
+	tutorial = true;	
+	time = 0;
 }
 
 function OnGUI()
 {
- 	GUI.Label (Rect (10, 10, 100, 20), timer1.ToString(), myStyle);
+ 	GUI.Label (Rect (10, 10, 100, 20), time.ToString(), myStyle);
+
+ 	if(tutorial == true)
+ 	{
+ 		GUI.Label (Rect (Screen.width/2-100, Screen.height/2-50, 500, 100), wasdImage);
+ 	}
 }
 
 function Update () {
 
-	TimerStart();
+	if(Input.anyKey && tutorial == true)
+	{
+		tutorial = false;
+		timer2 = Time.timeSinceLevelLoad;
+	}
 
+	if(tutorial == false)
+	{
+		TimerStart();
+	}
 }
 
 function TimerStart(){
 	 
-	 timer1 = Time.timeSinceLevelLoad;  //Set time
-
-	 Debug.Log(timer1);
+	 time = Time.timeSinceLevelLoad - timer2;  //Set time
  
+ }
+
+ function GetTime()
+ {
+ 	return time;
  }
